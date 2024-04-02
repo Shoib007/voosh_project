dab3426c4cd244ac2001a9e75212835ce9a6277a
Mohd Shoib <51898262+Shoib007@users.noreply.github.com>
2024-04-03 00:31:01 +0530

    Delete Account directory

--- Account/permissions.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsPublicProfileOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
-        return request.user.is_public
